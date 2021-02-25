9df50718a8591d8782a08fa37ba573c2ea4f1e3a
Wayne Huang <wayne.y.huang@gmail.com>
2021-02-25 12:58:27 -0800

    Initial Commit

---
+++ CustomViewExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomViewExample
+//
+//  Created by Wayne Huang on 2/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
